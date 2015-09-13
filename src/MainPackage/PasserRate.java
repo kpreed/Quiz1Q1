92c5a73b0e0f95da20cdb6817cf4a9791a0d44db
Kaelan Reed <kpreed@udel.edu>
2015-09-13 15:49:40 -0400

    Initial Commit

---
+++ src/MainPackage/PasserRate.java
@@ -0,0 +1,39 @@
+package MainPackage;
+
+import java.util.Scanner;
+
+public class PasserRate 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many touchdowns does the player have?");
+		String answer = input.nextLine();
+		double touchdowns = Double.parseDouble(answer);
+		
+		System.out.println("How many total yards does the player have?");
+		answer = input.nextLine();
+		double yards = Double.parseDouble(answer);
+		
+		System.out.println("How many interceptions has the player thrown?");
+		answer = input.nextLine();
+		double interceptions = Double.parseDouble(answer);
+		
+		System.out.println("How many completions has the player thrown?");
+		answer = input.nextLine();
+		double completions = Double.parseDouble(answer);
+		
+		System.out.println("How many passes has the player attempted?");
+		answer = input.nextLine();
+		double attempts = Double.parseDouble(answer);
+		
+		double a = (completions/attempts-0.3)*5;
+		double b = (yards/attempts-3)*0.25;
+		double c = (touchdowns/attempts)*20;
+		double d = 2.375 - (interceptions/attempts*25);
+		
+		double rating = ((a+b+c+d)/6)*100;
+		
+		System.out.printf("The players rating is: %.1f", rating);
+	}
+}
